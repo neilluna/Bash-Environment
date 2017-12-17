65799137c3924d51ddf69c2bf2a7b755fa8e3e44
Neil Luna <nluna@paraselene.com>
2017-12-17 10:14:54 -0600

    Cleaned up install script

--- install.sh
+++ install.sh
@@ -43,17 +43,17 @@ cp --no-preserve=mode "${1}" ~/"${1}"
 EOF1
 chmod u+x ${tmp_cp_script}
 
-# Find the Bash profile script.
-bash_profile=~/.bash_profile
+# Find the correct profile or login script.
+profile_script=~/.bash_profile
 if [ -f ~/.bash_profile ]; then
-  bash_profile=~/.bash_profile
+  profile_script=~/.bash_profile
 elif [ -f ~/.bash_login ]; then
-  bash_profile=~/.bash_login
+  profile_script=~/.bash_login
 elif [ -f ~/.profile ]; then
-  bash_profile=~/.profile
+  profile_script=~/.profile
 fi
 
-# Text lines that delimit the hooks in ~/.bash_profile and ~/.bashrc.
+# Text lines that delimit the ~/.bash_profile.d and ~/.bashrc.d script calls in ~/.bash_profile and ~/.bashrc.
 block_start='# Shell environment - Start of block'
 block_end='# Shell environment - End of block'
 block_warning='Do not remove or modify this line or any of the lines in this block.'
@@ -66,15 +66,17 @@ find .bash_profile.d -type d -exec mkdir -p ~/'{}' \;
 find .bash_profile.d -type f -exec ${tmp_cp_script} '{}' \;
 find ~/.bash_profile.d -type f -name '*.sh' -exec chmod u+x '{}' \;
 
-# Install or replace the ~/.bash_profile.d call in ~/.bash_profile.
-sed -i "/^${block_start}/,/^${block_end}/d" ${bash_profile}
-echo "${block_start} - ${block_warning}" >> ${bash_profile}
-cat << 'EOF2' >> ${bash_profile}
-for script in "${HOME}"/.bash_profile.d/*.sh; do
-  [ -f "${script}" ] && source "${script}"
-done
+# Install or replace the ~/.bash_profile.d script calls in the profile script.
+sed -i "/^${block_start}/,/^${block_end}/d" ${profile_script}
+echo "${block_start} - ${block_warning}" >> ${profile_script}
+cat << 'EOF2' >> ${profile_script}
+if [ -n "${BASH_VERSION}" ]; then
+  for script in "${HOME}"/.bash_profile.d/*.sh; do
+    [ -f "${script}" ] && source "${script}"
+  done
+fi
 EOF2
-echo "${block_end} - ${block_warning}" >> ${bash_profile}
+echo "${block_end} - ${block_warning}" >> ${profile_script}
 
 # Install or replace ~/.bashrc.d.
 if [ -d ~/.bashrc.d ]; then
@@ -84,7 +86,7 @@ find .bashrc.d -type d -exec mkdir -p ~/'{}' \;
 find .bashrc.d -type f -exec ${tmp_cp_script} '{}' \;
 find ~/.bashrc.d -type f -name '*.sh' -exec chmod u+x '{}' \;
 
-# Install or replace the ~/.bashrc.d call in ~/.bashrc.
+# Install or replace the ~/.bashrc.d script calls in ~/.bashrc.
 sed -i "/^${block_start}/,/^${block_end}/d" ~/.bashrc
 echo "${block_start} - ${block_warning}" >> ~/.bashrc
 cat << 'EOF3' >> ~/.bashrc
